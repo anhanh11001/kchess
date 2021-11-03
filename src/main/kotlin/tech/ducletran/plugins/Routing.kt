2075a09bc2379eb7f216a11943080f52f269cf6b
Duc Le Tran <letrananhduc0311@gmail.com>
2021-11-03 10:46:32 +0100

    Set up initial endpoints for the chess game

--- src/main/kotlin/tech/ducletran/plugins/Routing.kt
+++ src/main/kotlin/tech/ducletran/plugins/Routing.kt
@@ -6,11 +6,75 @@ import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
 
-fun Application.configureRouting() {
+private const val VERSION = 1
 
+fun Application.configureRouting() {
+    routeAuthentication()
+    routeUsers()
+    routeGames()
+    routeRooms()
+}
+
+fun Application.routeAuthentication() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        post("/v$VERSION/auth/login") {
+
+        }
+        post("/v$VERSION/auth/logout") {
+
+        }
+        post("/v$VERSION/auth/blacklist") { }
+    }
+}
+
+fun Application.routeUsers() {
+    routing {
+        // Create a new user login
+        post("/v$VERSION/users") {
+
+        }
+        // Win-loss history
+        get("/v$VERSION/users/{user_id}/history") {
+
+        }
+        get("/v$VERSION/users/{user_id}/profile") {
+
+        }
+    }
+}
+
+fun Application.routeRooms() {
+    routing {
+        // Get all rooms (public/private)
+        get("/rooms") {
+
+        }
+
+        // Create a new room
+        post("/v$VERSION/rooms") {
+
+        }
+
+        // Enter a room
+        post("/v$VERSION/rooms/{room-id}/enter") {
+
+        }
+    }
+}
+
+fun Application.routeGames() {
+    routing {
+        // Play the next move
+        post("/v$VERSION/games/move") {
+
+        }
+        // Undo
+        post("/v$VERSION/games/undo") {
+
+        }
+        // Game settings
+        post("/v$VERSION/games/settings") {
+
         }
     }
 }
